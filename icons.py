064f5b716d3e4a444a452c7acf59cf62fdb2ee82
Yellowbaron <43565545+Yellowbaron@users.noreply.github.com>
2021-02-14 18:13:45 +0300

    Delete icons.py

--- icons.py
+++
@@ -1,7 +0,0 @@
-material_icons = {
-                     'font-size': '3.5rem',
-                     'color': '#e0e5ec',
-                     'fill': '#e0e5ec',
-                     'filter': 'drop-shadow(3px 3px 2px #a3b1c6) drop-shadow(-3px -3px 2px #ffffff)',
-                     'margin': '1rem',
-}
